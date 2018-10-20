9069628dd8888d76be8fa097adb2d119385a328c
ElNiak <christophe.crochet@student.uclouvain.be>
2018-10-20 15:14:22 +0200

    v3

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,13 +1,16 @@
-public class Simulation {
-    public static void main(String args){
+import other.House;
 
+public class Simulation {
+    public static void main(String[] args){
+        scenario();
     }
 
-    public static void scenario1(){
+    public static void scenario(){
         System.out.println("Jean is watching TV during the night when suddenly he needs to go to the toilet,\n" +
                 " during this time, themovement sensor detect that he leaves the room and then switch off the\n " +
                 "TV and the lanmp.\n");
 
-
+        House house = new House();
+        house.toString();
     }
 }
