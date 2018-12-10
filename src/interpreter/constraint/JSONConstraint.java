652d9ae6abe5492ccee40ee878c6278b1875a480
ElNiak <christophe.crochet@student.uclouvain.be>
2018-12-10 11:29:56 +0100

    Interpreter Pattern Constraint - Manque plsu grand chose, juste les mthode pour analyser le json et pour envoyer les bon context ==> En gros le squellette est bon mais faut completer

---
+++ src/interpreter/constraint/JSONConstraint.java
@@ -0,0 +1,7 @@
+package interpreter.constraint;
+
+public class JSONConstraint {
+    public static boolean getValue(String json, String context, int section) {
+        return true; //Section == Room || sensor || software pour aller plus vite
+    }
+}
